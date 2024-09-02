5149987e0e52867ffe285ee2a4c8c98b39dd3d0e
Pasunuri Kathyayini <104193866+kathyayini-25@users.noreply.github.com>
2024-09-02 17:26:32 +0530

    Delete 1672-richest-customer-wealth directory

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        maxx=0
-        for i in range(len(accounts)):
-            if sum(accounts[i])>maxx:
-                maxx=sum(accounts[i])
-        return maxx
-            
